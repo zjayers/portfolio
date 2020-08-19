87d23132a46e197b317ea558642cb6ce7a799d03
Zach Ayers <ayers.dev@icloud.com>
2020-08-19 11:11:55 -0500

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+<h1 align="center">Welcome to gatsby-starter-hello-world 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/gatsbyjs/gatsby-starter-hello-world#readme" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://github.com/gatsbyjs/gatsby-starter-hello-world/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" />
+  </a>
+  <a href="https://github.com/gatsbyjs/gatsby-starter-hello-world/blob/master/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/gatsbyjs/gatsby-starter-hello-world" />
+  </a>
+</p>
+
+> A simplified bare-bones starter for Gatsby
+
+## Install
+
+```sh
+yarn install
+```
+
+## Usage
+
+```sh
+yarn run start
+```
+
+## Run tests
+
+```sh
+yarn run test
+```
+
+## Author
+
+- Website: https://gatsbyjs.org
+- Github: [@gatsbyjs](https://github.com/gatsbyjs)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/gatsbyjs/gatsby/issues). You can also take a look at the [contributing guide](https://github.com/gatsbyjs/gatsby-starter-hello-world/blob/master/CONTRIBUTING.md).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+This project is [MIT](https://github.com/gatsbyjs/gatsby-starter-hello-world/blob/master/LICENSE) licensed.
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/mock/data.js
@@ -0,0 +1,88 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Zach Ayers | Full Stack Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: "Zach Ayers's Portfolio", // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hi, my name is',
+  name: 'Zachariah Ayers',
+  subtitle: "I'm a Full-Stack Software Engineer",
+  cta: 'About Me',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: 'Zach is a software engineer specializing in JavaScript, Python, & C#.',
+  paragraphTwo:
+    'He has experience architecting applications on both the front and backend while managing a team of developers using Agile Software Development.',
+  paragraphThree:
+    'He has experience in cutting edge frameworks and technologies such as Angular, React, Node, and Django. An experienced engineer with a love for automation and robotics - he combines his analytical mind and creative ability to bring complex projects to life.',
+  resume: 'https://zjayers.github.io/resume/', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'natours.png',
+    title: 'Natours',
+    info:
+      'Nature trail application for booking, purchasing, and exploring user created trails around the globe!',
+    info2: 'React | Redux | Stripe | Node',
+    url: 'http://natours-ayers.herokuapp.com/',
+    repo: 'https://github.com/zjayers/natours', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'chalkboard.png',
+    title: 'Chalkboard',
+    info:
+      'Collaborative drawing applications for users to create, edit, and delete sketches in real time.',
+    info2: 'React | Socket.IO | Node',
+    url: 'https://ayers-chalkboard.herokuapp.com/',
+    repo: 'https://github.com/zjayers/chalkboard', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'cowaround.png',
+    title: 'Cow Around',
+    info: 'Light-hearted visualizer for creating, editing, and deleting temperamental cows.',
+    info2: 'React | Redux | MySQL | Node',
+    url: 'https://cowlist-ayers.herokuapp.com/',
+    repo: 'https://github.com/zjayers/hrr46-cowlist', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: 'I would love to work with you!',
+  btn: 'Contact Me',
+  email: 'z.ayers@icloud.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'http://www.linkedin.com/in/zjayers',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'http://www.github.com/zjayers',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
