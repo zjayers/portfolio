0170cb892224d8ed610b158f5049681904ccaea9
Zach Ayers <ayers.dev@icloud.com>
2021-01-26 10:48:58 -0600

    updates

--- src/mock/data.js
+++ src/mock/data.js
@@ -31,13 +31,23 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
+    img: 'lively.png',
+    title: 'Lively',
+    info: 'Social event application for sharing, attending, and following events / users.',
+    info2:
+      'ASP.NET Core 5 | Entity Framework | React | TypeScript | C# | Microsoft SQL Server | Azure',
+    url: 'https://livelyapp.azurewebsites.net',
+    repo: 'https://github.com/zjayers/asp.net.core.react',
+  },
+  {
+    id: nanoid(),
     img: 'natours.png',
     title: 'Natours',
     info:
       'Nature trail application for booking, purchasing, and exploring user created trails around the globe.',
     info2: 'React | Redux | Stripe | Express | JavaScript | MongoDB | Heroku',
     url: 'http://natours-ayers.herokuapp.com/',
-    repo: 'https://github.com/zjayers/natours', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/natours',
   },
   {
     id: nanoid(),
@@ -47,17 +57,7 @@ export const projectsData = [
       'Social application for posting, selling, and connecting with buyers of various products.',
     info2: 'React Native | Expo | Nestjs | TypeScript | MySQL | Github Actions',
     url: 'https://zjayers.github.io/host.donewithit/',
-    repo: 'https://github.com/zjayers/react.native.complete', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'lively.png',
-    title: 'Lively',
-    info: 'Social event application for sharing, attending, and following events / users.',
-    info2:
-      'ASP.NET Core 5 | Entity Framework | React | TypeScript | C# | Microsoft SQL Server | Azure',
-    url: 'https://livelyapp.azurewebsites.net',
-    repo: 'https://github.com/zjayers/asp.net.core.react', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/react.native.complete',
   },
   {
     id: nanoid(),
@@ -66,27 +66,7 @@ export const projectsData = [
     info: 'Popular Etherium/Solidity IDE ported to Mac Desktops (packaged with electron).',
     info2: 'Electron | Javascript | Etherium | Solidity | Blockchain ',
     url: 'https://github.com/zjayers/remix.ide/releases/tag/1.0',
-    repo: 'https://github.com/zjayers/remix.ide', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'hostify.png',
-    title: 'Hostify',
-    info: 'Front-end mockup for a web deployment/hosting service.',
-    info2: 'HTML | CSS | JavaScript',
-    url: 'https://zjayers.github.io/hostify/',
-    repo: 'https://github.com/zjayers/hostify', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'mfe.png',
-    title: 'Micro Front Ends',
-    info:
-      'Template application built with Webpack Module Federation - combining multiple front-end apps into a single container.',
-    info2:
-      'React | Vue | Webpack Module Federation | Material UI | JavaScript | Amazon S3 | Amazon Cloudfront | Github Actions',
-    url: 'https://dod1fwu8el2f8.cloudfront.net/',
-    repo: 'https://github.com/zjayers/mfe.complete', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/remix.ide',
   },
   {
     id: nanoid(),
@@ -97,7 +77,27 @@ export const projectsData = [
     info2: 'Vue | Chrome Plugin',
     url:
       'https://chrome.google.com/webstore/detail/vocation/icppkimiacdjokmejcmlhhdphlanokbo?hl=en',
-    repo: 'https://github.com/zjayers/proto.vue.jobscan', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/proto.vue.jobscan',
+  },
+  {
+    id: nanoid(),
+    img: 'hostify.png',
+    title: 'Hostify',
+    info: 'Front-end mockup for a web deployment/hosting service.',
+    info2: 'HTML | CSS | JavaScript',
+    url: 'https://zjayers.github.io/hostify/',
+    repo: 'https://github.com/zjayers/hostify',
+  },
+  {
+    id: nanoid(),
+    img: 'mfe.png',
+    title: 'Micro Front Ends',
+    info:
+      'Template application built with Webpack Module Federation - combining multiple front-end apps into a single container.',
+    info2:
+      'React | Vue | Webpack Module Federation | Material UI | JavaScript | Amazon S3 | Amazon Cloudfront | Github Actions',
+    url: 'https://dod1fwu8el2f8.cloudfront.net/',
+    repo: 'https://github.com/zjayers/mfe.complete',
   },
   {
     id: nanoid(),
@@ -107,7 +107,7 @@ export const projectsData = [
       'Model-View-Controller framework built in PHP 7.+ and deployed on LAMP stack EC2 instance.',
     info2: 'PHP | PDO | MySQL | Apache | Amazon EC2 | LAMP Stack',
     url: 'http://ec2-52-14-3-104.us-east-2.compute.amazonaws.com/php.mvc.blog/',
-    repo: 'https://github.com/zjayers/php.mvc.framework', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/php.mvc.framework',
   },
   {
     id: nanoid(),
@@ -116,7 +116,7 @@ export const projectsData = [
     info: 'Landing page for a modern real estate firm.',
     info2: 'HTML | SCSS | JavaScript',
     url: 'https://zjayers.github.io/design.nexter/',
-    repo: 'https://github.com/zjayers/design.nexter', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/design.nexter',
   },
   {
     id: nanoid(),
@@ -125,7 +125,7 @@ export const projectsData = [
     info: 'Landing page for hotel booking, messaging, notifications, and reviews.',
     info2: 'HTML | SCSS | JavaScript',
     url: 'https://zjayers.github.io/design.trillo/',
-    repo: 'https://github.com/zjayers/design.trillo', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/design.trillo',
   },
   {
     id: nanoid(),
@@ -134,7 +134,7 @@ export const projectsData = [
     info: 'Mockup landing page for viewing and booking campsites.',
     info2: 'Bootstrap | SCSS | JavaScript ',
     url: 'https://zjayers.github.io/design.bootstrap/',
-    repo: 'https://github.com/zjayers/design.bootstrap', // if no repo, the button will not show up
+    repo: 'https://github.com/zjayers/design.bootstrap',
   },
 ];
 
@@ -147,7 +147,7 @@ export const metricsData = {
   info2: 'PWA | Gatsby | React | GraphQL | Lighthouse ',
   url:
     'https://googlechrome.github.io/lighthouse/viewer/?psiurl=https%3A%2F%2Fzachayers.io%2F&strategy=mobile&category=performance&category=accessibility&category=best-practices&category=seo&category=pwa&utm_source=lh-chrome-ext',
-  repo: 'https://github.com/zjayers/portfolio', // if no repo, the button will not show up
+  repo: 'https://github.com/zjayers/portfolio',
 };
 
 // CONTACT DATA
